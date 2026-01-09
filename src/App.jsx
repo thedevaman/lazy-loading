502b784b380b391b156555beded2a8f9031e7da0
thedevaman <devaman2508@gmail.com>
2026-01-09 09:54:47 +0530

    base ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>Lazy Loading && Skeleton Loading</h1>
+    <div className='p-8'>
+      <h1 className='text-center font-bold text-4xl'>Lazy Loading & Skeleton Loading</h1>
+      <div className='grid grid-cols-3 space-x-2 space-y-2 mt-5'>
+        {
+        Array(20).fill("cghgh").map((item,index)=>(
+        <div key={index} className='bg-gray-200 rounded'>{item}</div>
+        ))
+        }
+      </div>
     </div>
   )
 }
