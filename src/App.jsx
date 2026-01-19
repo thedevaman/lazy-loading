2fe4c05e3124629d1602f2fd115d8cf723862fda
thedevaman <devaman2508@gmail.com>
2026-01-19 10:15:16 +0530

    skeleton added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
  
   const [images,setImages] = useState([])
   const [page,setPage] = useState(1)
+  const [load,setLoad] = useState(false)
 
   const fetchData = async ()=>{
  const res = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=6`)
@@ -25,7 +26,7 @@ function App() {
 
     if(ScrollableHeight <= windowHeight+scrollY)
     {
-  setPage(page+1)
+      setPage(page+1)
     }
 
 
@@ -48,10 +49,22 @@ function App() {
     <div className='p-8'>
       <h1 className='text-center font-bold text-4xl'>Lazy Loading & Skeleton Loading</h1>
       <div className='grid grid-cols-3 space-x-2 space-y-2 mt-5'>
+       {
+        !load && 
+        <div
+      style={{
+        width: "100%",
+        height: "100%",
+        background: "#e2e2e2",
+        animation: "pulse 1.5s infinite"
+      }}
+    ></div>
+}
+
         {
         images.map((item,index)=>(
         <div key={index} className='bg-gray-200 rounded'>
-          <img src={item.download_url} alt=""/>
+          <img src={item.download_url} alt="" loading="lazy"/>
         </div>
         ))
         }
